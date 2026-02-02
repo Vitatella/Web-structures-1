a0809edc9ed6733f8fd5e0dd780186d1fe2aebf8
Vitatella <116969929+Vitatella@users.noreply.github.com>
2026-02-02 11:14:20 +0700

    FGN

--- config/urls.py
+++ config/urls.py
@@ -22,6 +22,6 @@ from gallery.views import about
 urlpatterns = [
     path('admin/', admin.site.urls),
     # Пустая строка '' означает главную страницу сайта (http://localhost:8000/)
-    path('', home, name='home'),
-    path('about/', about, name='about'),
+    #path('', home, name='home'),
+    path('', about, name='about'),
 ]
