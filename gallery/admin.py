91a3a0d10583fca240767944d1da5c5f6fd40a02
Vitatella <116969929+Vitatella@users.noreply.github.com>
2026-02-09 10:47:06 +0700

    Lab 3 completed
    DB, models import, admin page

--- gallery/admin.py
+++ gallery/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Asset # Импортируем наш класс
+# Регистрируем
+admin.site.register(Asset)
--- gallery/views.py
+++ gallery/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Asset
 
 #def home(request):
     # Мы пока не используем HTML-шаблоны, просто вернем строку.
@@ -10,15 +11,10 @@ def about(request):
     return render(request, 'gallery/about.html')
 
 def home(request):
-    # Имитация данных из базы (список словарей)
-    fake_database = [
-        {'id': 1, 'name': 'Sci-Fi Helmet', 'file_size': '15 MB'},
-        {'id': 2, 'name': 'Old Chair', 'file_size': '2 MB'},
-        {'id': 3, 'name': 'Cyber Truck', 'file_size': '10 MB'},
-        {'id': 4, 'name': 'EWiobjboeihgwerh]', 'file_size': '1024 GB'},
-    ]
+    # ORM Запрос: "Дай мне все объекты Asset из базы"
+    assets = Asset.objects.all() 
     context_data = {
-        'page_title': 'Главная Галерея',
-        'assets': fake_database, # Передаем весь список
+    'page_title': 'Главная Галерея',
+    'assets': assets, # Передаем реальный QuerySet (список)
     }
     return render(request, 'gallery/index.html', context_data)
