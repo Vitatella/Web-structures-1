100f179715c07c7769501db1f64eb0d5be399cc1
Vitatella <116969929+Vitatella@users.noreply.github.com>
2026-02-11 14:32:01 +0700

    Lab 4 completed

--- gallery/views.py
+++ gallery/views.py
@@ -12,9 +12,12 @@ def about(request):
 
 def home(request):
     # ORM Запрос: "Дай мне все объекты Asset из базы"
-    assets = Asset.objects.all() 
+    assets = Asset.objects.all().order_by('-created_at')
     context_data = {
     'page_title': 'Главная Галерея',
-    'assets': assets, # Передаем реальный QuerySet (список)
+    'assets': assets
     }
     return render(request, 'gallery/index.html', context_data)
+
+def upload (request):
+    return render(request, 'gallery/upload.html')
