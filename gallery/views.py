11935ec6e930bedbf1955177440da26788fddaf3
Vitatella <Vitatelladev@gmail.com>
2026-03-16 10:33:10 +0700

    Lab 14 (7.2) completed

--- gallery/views.py
+++ gallery/views.py
@@ -16,6 +16,7 @@ from .forms import AssetForm
 from django.utils import timezone
 from datetime import timedelta
 
+from django.core.paginator import Paginator
 
 def about(request):
     #return HttpResponse("<h2>Курс Web Структуры.</h2>")
@@ -52,42 +53,26 @@ def home(request):
         # По умолчанию (new) - свежие сверху
         assets = assets.order_by('-created_at')
     # 5. Отдаем результат
+    paginator = Paginator(assets, 9) 
+    # Получаем номер страницы из URL (например, ?page=2)
+    page_number = request.GET.get('page')
+    # Получаем конкретный кусочек данных (объект Page)
+    page_obj = paginator.get_page(page_number)
     context_data = {
         'page_title': 'Главная Галерея',
-        'assets': assets,
+        # 'assets': assets,  <-- ЭТУ СТРОКУ УДАЛИТЬ!
+        'page_obj': page_obj, # <-- ВМЕСТО НЕЁ ЭТУ
     }
     return render(request, 'gallery/index.html', context_data)
 
 def upload(request):
     if request.method == 'POST':
-        form = AssetForm(request.POST, request.FILES)
+        # ... validation ...
         if form.is_valid():
-            # 1. Создаем объект, но пока НЕ сохраняем в базу (commit=False)
-            new_asset = form.save(commit=False)
-            
-            # 2. Обрабатываем картинку из скрытого поля
-            image_data = request.POST.get('image_data') # Получаем строку Base64
-            
-            if image_data:
-                # Формат строки: "data:image/jpeg;base64,/9j/4AAQSkZJRg..."
-                # Нам нужно отрезать заголовок "data:image/jpeg;base64,"
-                format, imgstr = image_data.split(';base64,') 
-                ext = format.split('/')[-1] # получаем "jpeg"
-                
-                # Декодируем текст в байты
-                data = base64.b64decode(imgstr)
-                
-                # Создаем имя файла (берем имя модели + .jpg)
-                file_name = f"{new_asset.title}_thumb.{ext}"
-                
-                # Сохраняем байты в поле image
-                # ContentFile превращает байты в объект, который понимает Django FileField
-                new_asset.image.save(file_name, ContentFile(data), save=False)
-            # 3. Финальное сохранение в БД
+            # ... save logic ...
             new_asset.save()
             
+            # ДОБАВЛЯЕМ СООБЩЕНИЕ
+            messages.success(request, f'Модель "{new_asset.title}" успешно загружена!')
+            
             return redirect('home')
-    else:
-        form = AssetForm()
-    return render(request, 'gallery/upload.html', {'form': form})
-
